29bf2850d6208148d7d37a4b480c08147c1e32f4
Abenezer <abenezerberhanu40@gmail.com>
2023-12-30 21:37:07 +0300

    finish

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import { client } from "@/lib/sanity";
+import { Button } from "@/components/ui/button";
+import { blogDetailData } from "@/lib/interface";
+import { client, urlFor } from "@/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 async function page({ params }: { params: { slug: string } }) {
@@ -10,15 +15,40 @@ async function page({ params }: { params: { slug: string } }) {
                 titleImage,
                 _id,
                 title,
+                content,
                 "slug": slug.current
             }[0]`;
 
     const data = await client.fetch(query);
     return data;
   };
-  const data = await getData();
-  console.log(data);
-  return <div></div>;
+  const data: blogDetailData = await getData();
+  return (
+    <div>
+      <h1>
+        <Button className="text-black bg-slate-200" asChild size={"sm"}>
+          <Link href={"/"}>{"<"}Back</Link>
+        </Button>
+        <span className="block text-base text-center text-primary font-bold dark:font-semibold tracking-wide uppercase">
+          Jan Marshal - Blog
+        </span>
+        <span className="mt-4 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+        <Image
+          src={urlFor(data.titleImage).url()}
+          alt="image"
+          width={500}
+          height={500}
+          priority
+          className="w-[500px] h-[200px] sm:h-[400px] mx-auto my-5 rounded-lg border"
+        />
+      </h1>
+      <div className="mt-16 prose prose-blue decoration-transparent dark:prose-invert prose-xl">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
 }
 
 export default page;
--- lib/interface.ts
+++ lib/interface.ts
@@ -1,7 +1,17 @@
 export interface homeBlogData {
-    title: string;
-    description: string;
-    _id: string;
-    slug: string;
-    titleImage:any
-  }
+  title: string;
+  description: string;
+  _id: string;
+  slug: string;
+  titleImage: any;
+}
+
+export interface blogDetailData {
+  titleImage: any;
+  _id: string;
+  title: string;
+  content: any;
+  slug: string;
+  _createdAt: string;
+  description: string;
+}
